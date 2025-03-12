895a877a73e049226b95d303fa578077b0d83f2b
namassist <chairulanmm@gmail.com>
2025-03-12 21:47:21 +0700

    [fix] update footer copyright year dynamically

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,7 +5,10 @@ export default function Footer() {
     <footer>
       <div className="container mx-auto">
         <div className="w-full flex flex-col lg:flex-row justify-between lg:items-center text-sm font-secondary-sans py-5 border-neutral-800 border-t-2">
-          <p>© 2024 | Chairul An&apos;aam Maulidin. Copyright protected.</p>
+          <p>
+            © {new Date().getFullYear()} | Chairul An&apos;aam Maulidin.
+            Copyright protected.
+          </p>
           <p>Made with &#10084; in Indonesia</p>
         </div>
       </div>
